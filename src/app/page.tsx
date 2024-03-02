ef452a305690bbb0161466df6292afc1eab51961
wave-nature <vs1010582@gmail.com>
2024-03-02 19:57:51 +0530

    init next project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,195 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
-export default function Home() {
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  const [url, setUrl] = useState("");
+  const [word, setWord] = useState("");
+  const [result, setResult] = useState<any>(null);
+  const [total, setTotal] = useState(0);
+  const [occurrence, setOccurrence] = useState(1);
+
+  useEffect(() => {
+    const getUrl = async () => {
+      const tabs = await chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true,
+      });
+      const currentUrl = tabs[0].url;
+      if (currentUrl?.includes("youtube") && currentUrl?.includes("watch")) {
+        if (currentUrl.includes("&t=")) {
+          const url = currentUrl.split("&t=")[0];
+          setUrl(url);
+        } else {
+          setUrl(currentUrl);
+        }
+      }
+    };
+    getUrl();
+  }, []);
+
+  async function searchWord(e: React.FormEvent) {
+    e.preventDefault();
+    if (!url) return toast.error("Youtube URL is required");
+    if (!word) return toast.error("Word to search is required");
+
+    try {
+      toast.loading("Searching for the word in video");
+      const res = await fetch("http://localhost:7000/video-text", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ url }),
+      });
+      if (!res.ok) return;
+      const data = await res.json();
+      console.log(data);
+      toast.remove();
+      const newResult = [];
+
+      for (let i = 0; i < data.length; i++) {
+        const obj = data[i];
+        const text = obj.text?.toLowerCase()?.trim();
+        if (text.includes(word?.toLowerCase()?.trim())) {
+          newResult.push(obj);
+        }
+      }
+
+      setResult(newResult);
+      if (newResult.length > 0) {
+        setTotal(newResult.length);
+        toast.success("Word found in video");
+      } else {
+        toast.error("Word not found in video");
+      }
+    } catch (error) {
+      toast.error("Error in searching the word");
+    }
+  }
+
+  const occurred = result?.[occurrence - 1];
+  let timeStampMinutes = 0;
+  let timestampSeconds = Math.ceil(occurred?.offset / 1000);
+  if (timestampSeconds > 60) {
+    timeStampMinutes = Math.floor(timestampSeconds / 60);
+    timestampSeconds = timestampSeconds % 60;
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="main">
+      <header className="logo-container">
+        <div className="logo">Worvid</div>
+      </header>
+
+      <Card>
+        <div className="search-box">
+          <h4>Oh! you just remember a word</h4>
+          <h4>Don't worry search where it exactly is in video!</h4>
+
+          <form className="form" onSubmit={searchWord}>
+            <div className="input-group">
+              <label htmlFor="yt-url">Youtube URL:</label>
+              <input
+                id="yt-url"
+                type="text"
+                placeholder="Youtube URL"
+                value={url}
+                onChange={(e) => setUrl(e.target.value.trim())}
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="search-word">Word:</label>
+              <input
+                id="search-word"
+                type="text"
+                placeholder="You know the text"
+                value={word}
+                onChange={(e) => setWord(e.target.value)}
+              />
+            </div>
+
+            <div className="btn-container">
+              <button className="btn">Search</button>
+            </div>
+          </form>
         </div>
-      </div>
+      </Card>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {result && result.length > 0 && (
+        <Card>
+          <div className="search-result">
+            <h4>
+              "{word}" occurs <span>{result?.length} times</span> in this video
+            </h4>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+            <div className="search-result-container">
+              <div className="occurrence">
+                <h4>Occurrence</h4>
+                <div className="occ-input">
+                  <input type="number" value={occurrence} />
+                  <div className="arrow-container">
+                    <button
+                      className="btn-up"
+                      onClick={() => {
+                        if (occurrence < total) setOccurrence(occurrence + 1);
+                      }}
+                    ></button>
+                    <button
+                      className="btn-down"
+                      onClick={() => {
+                        if (occurrence > 1) setOccurrence(occurrence - 1);
+                      }}
+                    ></button>
+                  </div>
+                </div>
+              </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+              <div className="timestamp">
+                <h4>Timestamp</h4>
+                <div>
+                  {timeStampMinutes <= 0 ? "00" : timeStampMinutes}:
+                  {timestampSeconds < 10
+                    ? `0${timestampSeconds}`
+                    : timestampSeconds}
+                  s
+                </div>
+              </div>
+            </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+            <div className="go-to">
+              Go to this timestamp or copy the url below
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <div className="copy-url">
+            <div>{`${url}&t=${Math.ceil(
+              (occurred?.offset || 0) / 1000
+            )}s`}</div>
+            <button
+              id="copy-url"
+              className="btn"
+              onClick={() => {
+                navigator.clipboard.writeText(
+                  `${url}&t=${Math.ceil((occurred?.offset || 0) / 1000)}s`
+                );
+                const copyBtn = document.getElementById("copy-url");
+                if (copyBtn) copyBtn.innerText = "Copied!";
+                toast.success("URL copied to clipboard");
+                setTimeout(() => {
+                  if (copyBtn) copyBtn.innerText = "Copy";
+                }, 2000);
+              }}
+            >
+              Copy
+            </button>
+          </div>
+        </Card>
+      )}
+      <Toaster />
     </main>
   );
 }
+export default App;
